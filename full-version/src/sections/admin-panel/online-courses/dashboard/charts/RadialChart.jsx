2479606dfd5258b1b5d26363d5e4e714201e8d44
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2025-11-15 00:19:05 +0700

    insert-able-template

---
+++ full-version/src/sections/admin-panel/online-courses/dashboard/charts/RadialChart.jsx
@@ -0,0 +1,51 @@
+'use client';
+import { useState } from 'react';
+
+// next
+import dynamic from 'next/dynamic';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+
+const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+// ==============================|| INVITE GOAL - CHART ||============================== //
+
+export default function RadialBarChart() {
+  const theme = useTheme();
+
+  const [options] = useState({
+    chart: {
+      type: 'radialBar',
+      offsetY: -40
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -90,
+        endAngle: 90,
+        hollow: {
+          size: '70%',
+          background: 'transparent'
+        },
+        track: {
+          background: theme.palette.grey[200],
+          strokeWidth: '50%'
+        },
+        dataLabels: {
+          name: { show: false },
+          value: {
+            offsetY: -30,
+            fontSize: '24px',
+            color: theme.palette.text.primary
+          }
+        }
+      }
+    },
+    stroke: { lineCap: 'round', width: 20 },
+    colors: [theme.palette.primary.main]
+  });
+
+  const [series] = useState([75.55]);
+
+  return <ReactApexChart options={options} series={series} type="radialBar" height={350} />;
+}
---
+++ full-version/src/sections/widget/chart/EcommerceRadial.jsx
@@ -0,0 +1,106 @@
+'use client';
+import PropTypes from 'prop-types';
+
+import { useState, useEffect } from 'react';
+
+// next
+import dynamic from 'next/dynamic';
+
+// material-ui
+import { alpha, useTheme } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+// project-imports
+import MainCard from 'components/MainCard';
+import { ThemeMode } from 'config';
+
+const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+// ==============================|| CHART ||============================== //
+
+function EcommerceDataChart({ color }) {
+  const theme = useTheme();
+  const mode = theme.palette.mode;
+  color = color === 'error.dark' ? theme.palette.error.dark : theme.palette.primary.main;
+
+  // chart options
+  const areaChartOptions = {
+    chart: {
+      id: 'new-stack-chart',
+      type: 'radialBar'
+    },
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          margin: 0,
+          size: '60%',
+          background: 'transparent',
+          imageOffsetX: 0,
+          imageOffsetY: 0,
+          position: 'front'
+        },
+        track: {
+          background: alpha(color, 0.5),
+          strokeWidth: '50%'
+        },
+
+        dataLabels: {
+          show: true,
+          name: {
+            show: false
+          },
+          value: {
+            offsetY: 7,
+            color: color,
+            fontSize: '20px',
+            fontWeight: '700',
+            show: true
+          }
+        }
+      }
+    }
+  };
+
+  const { primary, secondary } = theme.palette.text;
+  const line = theme.palette.divider;
+
+  const [options, setOptions] = useState(areaChartOptions);
+
+  useEffect(() => {
+    setOptions((prevState) => ({
+      ...prevState,
+      colors: [color],
+      theme: {
+        mode: mode === ThemeMode.DARK ? 'dark' : 'light'
+      }
+    }));
+  }, [color, mode, primary, secondary, line, theme]);
+
+  const [series] = useState([30]);
+
+  return <ReactApexChart options={options} series={series} type="radialBar" height={140} />;
+}
+
+// ==============================|| CHART WIDGET - ECOMMERCE RADIAL  ||============================== //
+
+export default function EcommerceRadial({ color }) {
+  return (
+    <MainCard content={false} sx={{ height: 1 }}>
+      <Stack direction="row" sx={{ gap: 2, alignItems: 'center', height: 1, px: 2 }}>
+        <Box sx={{ width: 135 }}>
+          <EcommerceDataChart color={color} />
+        </Box>
+        <Stack>
+          <Typography>Total Earning</Typography>
+          <Typography variant="subtitle1">$45,890</Typography>
+        </Stack>
+      </Stack>
+    </MainCard>
+  );
+}
+
+EcommerceDataChart.propTypes = { color: PropTypes.string };
+
+EcommerceRadial.propTypes = { color: PropTypes.string };
